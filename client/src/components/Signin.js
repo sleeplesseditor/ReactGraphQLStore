74abaa2fc63e5f4129e6ec3beeb81ff3d617ac7f
sleeplesseditor <josh@bryson.me.uk>
2018-12-21 15:59:55 +0000

    Initial React route setup

---
+++ client/src/components/Signin.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Signin extends Component {
+    render() {
+        return (
+            <div>
+                Sign in
+            </div>
+        )
+    }
+}
+
+export default Signin;
---
+++ client/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './index.css';
+import App from './components/App';
+import Checkout from './components/Checkout';
+import Signup from './components/Signup';
+import Signin from './components/Signin';
+import * as serviceWorker from './serviceWorker';
+
+const Root = () => (
+    <Router>
+        <Switch>
+            <Route exact path="/" component={App} />
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/signin" component={Signin} />
+            <Route path="/signup" component={Signup} />
+        </Switch>
+    </Router>
+)
+
+ReactDOM.render(<Root />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
