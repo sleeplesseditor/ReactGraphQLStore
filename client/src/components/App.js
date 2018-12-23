428d827ede144260c76690aa50c594cc752926b3
sleeplesseditor <josh@bryson.me.uk>
2018-12-23 16:54:47 +0000

    Local state setup

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,26 +7,34 @@ const apiUrl = process.env.API_URL || 'http://localhost:1337';
 const strapi = new Strapi(apiUrl);
 
 class App extends Component {
+
+  state = {
+    brands: []
+  };
   
   async componentDidMount() {
-    const response = await strapi.request('POST', '/graphql', {
-      data: {
-        query: `
-          query {
-            brands {
-              _id
-              name 
-              description
-              createdAt
-              image {
-                url
-                name
+    try {
+      const { data } = await strapi.request('POST', '/graphql', {
+        data: {
+          query: `
+            query {
+              brands {
+                _id
+                name 
+                description
+                image {
+                  url
+                }
               }
-            }
-          }`
-      }
-    });
-    console.log(response);
+            }`
+        }
+      });
+      this.setState({
+        brands: data.brands
+      });
+    } catch (err) {
+      console.error(err);
+    }
   }
 
   render() {
