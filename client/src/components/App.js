ee1df1316ac1566e45a3ebac6e6e2984b8912b3b
sleeplesseditor <josh@bryson.me.uk>
2018-12-24 14:17:45 +0000

    Brew card component created

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Box, Container, Heading } from 'gestalt';
+import { Box, Card, Container, Heading, Image, Text } from 'gestalt';
+import { Link } from 'react-router-dom';
 import './App.css';
 import Strapi from 'strapi-sdk-javascript/build/main';
 
@@ -38,6 +39,7 @@ class App extends Component {
   }
 
   render() {
+    const { brands } = this.state;
     return (
       <Container>
         <Box
@@ -51,6 +53,58 @@ class App extends Component {
           >
             Brew Brands
           </Heading>
+        </Box>
+        <Box
+          dangerouslySetInlineStyle={{
+            __style: {
+              backgroundColor: '#d6c8ec'
+            }
+          }}
+          shape="rounded"
+          wrap
+          display="flex"
+          justifyContent="around"
+        >
+          {brands.map(brand => (
+            <Box
+              paddingY={4}
+              margin={2}
+              width={200}
+              key={brand._id}
+            >
+              <Card
+                image={
+                  <Box
+                    height={200}
+                    width={200}
+                  >
+                    <Image 
+                      fit="cover"
+                      alt="Brand"
+                      naturalHeight={1}
+                      naturalWidth={1}
+                      src={`${apiUrl}${brand.image.url}`}
+                    />
+                  </Box>
+                }
+              >
+              <Box
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                direction="column"
+              >
+                <Text bold size="xl">{brand.name}</Text>
+                <Text>{brand.description}</Text>
+                <Text bold size="xl">
+                  <Link to={`/${brand._id}`}>
+                    See Brews
+                  </Link>
+                </Text>
+              </Box>
+              </Card>
+            </Box> 
+          ))}
         </Box>
       </Container>
     );
