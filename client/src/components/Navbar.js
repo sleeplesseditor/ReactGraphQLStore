680dd3f0b04b74d8b1ab7439a2f2cdb279ec8f80
sleeplesseditor <josh@bryson.me.uk>
2018-12-27 13:08:50 +0000

    Sign out and sign in functionality added

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,8 +1,80 @@
-import React from 'react';
-import { Box, Heading, Image } from 'gestalt';
-import { NavLink } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Box, Button, Heading, Image } from 'gestalt';
+import { clearCart, clearToken, getToken } from '../utils';
+import { NavLink, withRouter } from 'react-router-dom';
 
-const Navbar = () => {
+class Navbar extends Component {
+    handleSignOut = () => {
+        clearCart();
+        clearToken();
+        this.props.history.push('/');
+    }
+
+    render() {
+        return getToken() !== null ? <AuthNav handleSignOut={this.handleSignOut} /> : <UnAuthNav />;
+    }
+}
+
+const AuthNav = ({ handleSignOut }) => {
+    return (
+        <Box
+            display="flex"
+            alignItems="center"
+            justifyContent="around"
+            height={70}
+            color="midnight"
+            padding={1}
+            shape="roundedBottom"
+        >
+            <NavLink 
+                activeClassName="active"
+                className="navbar_link"
+                to="/checkout"
+            >
+                    Checkout
+            </NavLink>
+
+            <NavLink 
+                exact
+                to="/"
+            >
+                <Box
+                    display="flex"
+                    alignItems="center"
+                    margin={2}
+                >
+                    <Box
+                        height={50}
+                        width={50}
+                    >
+                        <Image 
+                            alt="Brewhaha Logo"
+                            naturalHeight={1}
+                            naturalWidth={1}
+                            src="./icons/logo.svg"
+                        />
+                    </Box>
+                    <Heading
+                        size="xs"
+                        color="orange"
+                    >
+                        BrewHaha
+                    </Heading>
+                </Box>
+            </NavLink>
+            <Button 
+                onClick={handleSignOut}
+                color="red"
+                text="Sign Out"
+                inline
+                size="md"
+
+            />
+        </Box>
+    )
+}
+
+const UnAuthNav = () => {
     return (
         <Box
             display="flex"
@@ -61,4 +133,4 @@ const Navbar = () => {
     )
 }
 
-export default Navbar;
+export default withRouter(Navbar);
--- client/src/components/Signin.js
+++ client/src/components/Signin.js
@@ -1,11 +1,145 @@
 import React, { Component } from 'react';
+import { Box, Button, Container, Heading, TextField } from 'gestalt';
+import { setToken } from '../utils';
+import ToastMessage from './ToastMessage';
+import Strapi from 'strapi-sdk-javascript/build/main';
+
+const apiUrl = process.env.API_URL || 'http://localhost:1337';
+const strapi = new Strapi(apiUrl);
 
 class Signin extends Component {
+
+    state = {
+        username: '',
+        password: '',
+        toast: false,
+        toastMessage: '',
+        loading: false
+    }
+
+    handleChange = ({ event, value }) => {
+        event.persist();
+        this.setState({
+            [event.target.name]: value
+        });
+    }
+
+    handleSubmit = async event => {
+        const { username, password } = this.state;
+        event.preventDefault();
+
+        if(this.isFormEmpty(this.state)){
+            this.showToast('Please fill in all fields to sign up');
+            return;
+        } 
+        
+        try {
+            this.setState({ loading: true });
+            const response = await strapi.login(username, password);
+            this.setState({ loading: false });
+            setToken(response.jwt);
+            console.log(response);
+            this.redirectUser('/');
+        } catch (err) {
+            this.setState({ loading: false });
+            this.showToast(err.message);
+        }
+    };
+
+    redirectUser = path => this.props.history.push(path);
+
+    isFormEmpty = ({ username, password }) => {
+        return !username || !password;
+    };
+
+    showToast = toastMessage => {
+        this.setState({
+            toast: true,
+            toastMessage
+        });
+        setTimeout(() => this.setState({ 
+            toast: false,
+            toastMessage: '' 
+        }), 5000);
+    }
+
     render() {
+        const { toastMessage, toast, loading } = this.state;
+
         return (
-            <div>
-                Sign in
-            </div>
+            <Container>
+                <Box
+                    dangerouslySetInlineStyle={{
+                        __style: {
+                            backgroundColor: "#bdcdd9"
+                        }
+                    }}
+                    margin={4}
+                    padding={4}
+                    shape="rounded"
+                    display="flex"
+                    justifyContent="center"
+                >
+                    <form 
+                        style={{
+                            display: 'inlineBlock',
+                            textAlign: 'center',
+                            maxWidth: 450
+                        }}
+                        onSubmit={this.handleSubmit}
+                    >
+                        <Box
+                            marginBottom={2}
+                            display="flex"
+                            direction="column"
+                            alignItems="center"
+                        >
+                            <Heading
+                                color="midnight"
+                            >
+                                Welcome Back
+                            </Heading>
+                            <Box
+                                marginTop={2}
+                            >
+                                <TextField 
+                                    id="username"
+                                    type="text"
+                                    name="username"
+                                    placeholder="Username"
+                                    onChange={this.handleChange}
+                                />
+                            </Box>
+                            <Box
+                                marginTop={2}
+                            >
+                                <TextField 
+                                    id="password"
+                                    type="password"
+                                    name="password"
+                                    placeholder="Password"
+                                    onChange={this.handleChange}
+                                />
+                            </Box>
+                            <Box
+                                marginTop={2}
+                            >
+                                <Button 
+                                    inline
+                                    disabled={loading}
+                                    color="blue"
+                                    text="Submit"
+                                    type="submit"
+                                />
+                            </Box>
+                        </Box>
+                    </form>
+                </Box>
+                <ToastMessage 
+                    show={toast}
+                    message={toastMessage}
+                />
+            </Container>
         )
     }
 }
