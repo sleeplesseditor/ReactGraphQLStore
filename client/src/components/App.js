448a7bb3fc2fea3740c1a022771c19603ccc8e75
sleeplesseditor <josh@bryson.me.uk>
2019-01-01 11:12:30 +0000

    Search function updatd to GraphQL

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -46,18 +46,42 @@ class App extends Component {
   }
 
   handleChange = ({ value }) => {
-    this.setState({ searchTerm: value });
+    this.setState({ searchTerm: value }, () => this.searchBrands());
   }
 
-  filteredBrands = ({ brands, searchTerm }) => {
-    return brands.filter(brand => {
-      return brand.name.toLowerCase().includes(searchTerm.toLowerCase()) || 
-      brand.description.toLowerCase().includes(searchTerm.toLowerCase());
+  // filteredBrands = ({ brands, searchTerm }) => {
+  //   return brands.filter(brand => {
+  //     return brand.name.toLowerCase().includes(searchTerm.toLowerCase()) || 
+  //     brand.description.toLowerCase().includes(searchTerm.toLowerCase());
+  //   });
+  // };
+
+  searchBrands = async () => {
+    const response = await strapi.request('POST', '/graphql', {
+      data: {
+        query: `query {
+          brands(where: {
+            name_contains: "${this.state.searchTerm}"
+          }) {
+            _id
+            name
+            description
+            image {
+              url
+            }
+          }
+        }`
+      }
     });
-  };
+    console.log(this.state.searchTerm, response.data.brands);
+    this.setState({
+      brands: response.data.brands,
+      loadingBrands: false
+    });
+  }
 
   render() {
-    const { searchTerm, loadingBrands } = this.state;
+    const { searchTerm, loadingBrands, brands } = this.state;
     return (
       <Container>
         <Box
@@ -108,7 +132,7 @@ class App extends Component {
           display="flex"
           justifyContent="around"
         >
-          {this.filteredBrands(this.state).map(brand => (
+          {brands.map(brand => (
             <Box
               paddingY={4}
               margin={2}
